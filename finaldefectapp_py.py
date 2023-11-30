e5c04cdfe68670bceed72a6ff5de0ca8989a5c32
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-11-30 22:28:47 +0200

    Update finaldefectapp_py.py

--- finaldefectapp_py.py
+++ finaldefectapp_py.py
@@ -4,12 +4,23 @@ import os
 from pathlib import Path
 from keras.preprocessing import image
 import numpy as np
-from keras.models import load_model
 from skimage.metrics import structural_similarity as ssim
-from skimage import io, color, transform
+from PIL import Image
+
+# Function to calculate Structural Similarity Index (SSI)
+def calculate_ssim(img_path, reference_img_path):
+    img = Image.open(img_path).convert('L')  # Convert to grayscale
+    reference_img = Image.open(reference_img_path).convert('L')
+
+    img_array = np.array(img)
+    reference_img_array = np.array(reference_img)
+
+    return ssim(img_array, reference_img_array)
 
 # Function to load the trained MobileNet model
-def load_mobilenet_model(model_path):
+def load_mobilenet_model():
+    model_path = 'mobilenet_model (1).h5'
+
     try:
         model = load_model(model_path)
         return model
@@ -17,37 +28,14 @@ def load_mobilenet_model(model_path):
         st.error(f"Error loading the model: {str(e)}")
         return None
 
-# Function to preprocess images and resize them
-def preprocess_image(image_path, target_size=(150, 150)):
-    img = image.load_img(image_path, target_size=target_size)
+# Function to make predictions
+def predict_defect(image_path, model):
+    img = image.load_img(image_path, target_size=(150, 150))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0) / 255.0
-    return img_array
 
-# Function to calculate Structural Similarity Index (SSI)
-# Function to calculate Structural Similarity Index (SSI)
-def calculate_ssim(reference_image, uploaded_image_path):
-    # Read the reference image
-    reference_img = io.imread(reference_image)
-    
-    # Convert images to grayscale
-    reference_gray = color.rgb2gray(reference_img)
-    
-    # Read the uploaded image and resize it to match the reference image dimensions
-    uploaded_img = preprocess_image(uploaded_image_path, target_size=reference_img.shape[:2])
-    
-    # Convert the resized image to grayscale
-    uploaded_gray = color.rgb2gray(uploaded_img[0])
-    
-    # Ensure images have the same shape
-    if reference_gray.shape != uploaded_gray.shape:
-        raise ValueError("Reference and uploaded images must have the same shape.")
-    
-    # Calculate SSIM
-    index = ssim(reference_gray, uploaded_gray)
-    
-    return index
-
+    prediction = model.predict(img_array)
+    return prediction
 
 # Function to assess the highest probability predicted and print out the class of the image
 def assess_defect(prediction, classes):
@@ -63,42 +51,44 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
     if uploaded_file is not None:
-        # Create a temporary directory if it doesn't exist
-        temp_dir = 'temp'
-        os.makedirs(temp_dir, exist_ok=True)
+        # Calculate SSIM with the reference image (rusty metal.jpg)
+        reference_image_path = 'rusty metal.jpg'
+        ssim_value = calculate_ssim(uploaded_file, reference_image_path)
 
-        # Create a path for the temporary image
-        temp_path = os.path.join(temp_dir, 'temp_image.jpg')
-        uploaded_file.seek(0)
-        with open(temp_path, 'wb') as f:
-            f.write(uploaded_file.read())
-
-        # Reference image for SSI
-        reference_image = 'inclusion.jpg'
-
-        # Calculate SSIM
-        ssim_index = calculate_ssim(reference_image, temp_path)
+        st.write(f"SSIM with the reference image: {ssim_value}")
 
         # Set a threshold for SSIM
         ssim_threshold = 0.7  # Adjust the threshold as needed
 
-        if ssim_index > ssim_threshold:
-            # Load the model for defect assessment
-            model_path = 'mobilenet_model (1).h5'
-            defect_model = load_mobilenet_model(model_path)
+        if ssim_value > ssim_threshold:
+            # Continue with defect assessment
 
-            if defect_model is not None:
-                # Make predictions for defects
-                prediction = predict_defect(temp_path, defect_model)
+            # Create a temporary directory if it doesn't exist
+            temp_dir = 'temp'
+            os.makedirs(temp_dir, exist_ok=True)
+
+            # Create a path for the temporary image
+            temp_path = os.path.join(temp_dir, 'temp_image.jpg')
+            uploaded_file.seek(0)
+            with open(temp_path, 'wb') as f:
+                f.write(uploaded_file.read())
+
+            # Load the model
+            model = load_mobilenet_model()
+
+            if model is not None:
+                # Make predictions
+                prediction = predict_defect(temp_path, model)
 
                 # Display the results
                 st.subheader("Prediction Results:")
                 for i, class_name in enumerate(classes):
-                    st.write(f"{class_name}: {prediction[i]}")
+                    st.write(f"{class_name}: {prediction[0][i]}")
 
                 # Assess the highest probability predicted and print out the class
-                max_prob_class = assess_defect(prediction, classes)
+                max_prob_class = assess_defect(prediction[0], classes)
                 st.success(f"This metal surface has a defect of: {max_prob_class}")
+
             else:
                 st.error("Defect assessment model not loaded.")
         else:
