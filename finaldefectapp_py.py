e949ab2e3190ee8760f1af76fc5c67b7ee371dee
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-11-30 21:42:09 +0200

    Update finaldefectapp_py.py

--- finaldefectapp_py.py
+++ finaldefectapp_py.py
@@ -4,32 +4,12 @@ import os
 from pathlib import Path
 from keras.preprocessing import image
 import numpy as np
-from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+from keras.models import load_model
+from skimage.metrics import structural_similarity as ssim
+from skimage import io, color
 
-# Function to check if the image contains metallic surfaces using MobileNetV2
-def contains_metallic_surface(image_path, threshold=0.5):
-    model = MobileNetV2(weights='imagenet')
-
-    img = image.load_img(image_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
-
-    predictions = model.predict(img_array)
-    decoded_predictions = decode_predictions(predictions)
-
-    metallic_score = 0.0
-    for _, _, score in decoded_predictions[0]:
-        if 'metal' in _:
-            metallic_score = score
-            break
-
-    return metallic_score > threshold
-
-# Function to load the trained MobileNet model for defect assessment
-def load_mobilenet_model():
-    model_path = 'mobilenet_model (1).h5'
-
+# Function to load the trained MobileNet model
+def load_mobilenet_model(model_path):
     try:
         model = load_model(model_path)
         return model
@@ -37,14 +17,22 @@ def load_mobilenet_model():
         st.error(f"Error loading the model: {str(e)}")
         return None
 
-# Function to make predictions for defects
-def predict_defect(image_path, model):
-    img = image.load_img(image_path, target_size=(150, 150))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
-
-    prediction = model.predict(img_array)
-    return prediction
+# Function to calculate Structural Similarity Index (SSI)
+def calculate_ssim(reference_image, image_path):
+    # Read the reference image
+    reference_img = io.imread(reference_image)
+    
+    # Read the uploaded image
+    uploaded_img = io.imread(image_path)
+    
+    # Convert images to grayscale
+    reference_gray = color.rgb2gray(reference_img)
+    uploaded_gray = color.rgb2gray(uploaded_img)
+    
+    # Calculate SSIM
+    index, _ = ssim(reference_gray, uploaded_gray, full=True)
+    
+    return index
 
 # Function to assess the highest probability predicted and print out the class of the image
 def assess_defect(prediction, classes):
@@ -70,27 +58,36 @@ def main():
         with open(temp_path, 'wb') as f:
             f.write(uploaded_file.read())
 
-        # Check if the image contains metallic surfaces
-        if contains_metallic_surface(temp_path):
-            # Load the defect assessment model
-            model = load_mobilenet_model()
+        # Reference image for SSI
+        reference_image = 'rusty_metal.jpg'
 
-            if model is not None:
+        # Calculate SSIM
+        ssim_index = calculate_ssim(reference_image, temp_path)
+
+        # Set a threshold for SSIM
+        ssim_threshold = 0.7  # Adjust the threshold as needed
+
+        if ssim_index > ssim_threshold:
+            # Load the model for defect assessment
+            model_path = 'mobilenet_model (1).h5'
+            defect_model = load_mobilenet_model(model_path)
+
+            if defect_model is not None:
                 # Make predictions for defects
-                prediction = predict_defect(temp_path, model)
+                prediction = predict_defect(temp_path, defect_model)
 
                 # Display the results
                 st.subheader("Prediction Results:")
                 for i, class_name in enumerate(classes):
-                    st.write(f"{class_name}: {prediction[0][i]}")
+                    st.write(f"{class_name}: {prediction[i]}")
 
                 # Assess the highest probability predicted and print out the class
-                max_prob_class = assess_defect(prediction[0], classes)
+                max_prob_class = assess_defect(prediction, classes)
                 st.success(f"This metal surface has a defect of: {max_prob_class}")
             else:
                 st.error("Defect assessment model not loaded.")
         else:
-            st.warning("The uploaded image does not appear to contain metallic surfaces.")
+            st.warning("The uploaded image does not appear to contain a metallic surface.")
 
 # Define your classes
 classes = ['Crazing', 'Inclusion', 'Patches', 'Pitted', 'Rolled', 'Scratches']
