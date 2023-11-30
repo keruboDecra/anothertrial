7c3e07fbf4a1a7f4f4354b52ba4e8d5cf5c8b4a8
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-11-30 20:41:33 +0200

    Update finaldefectapp_py.py

--- finaldefectapp_py.py
+++ finaldefectapp_py.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from keras.preprocessing import image
 import numpy as np
 from keras.models import load_model
-import cv2
 
 
 # Function to check if the image contains metallic surfaces (example using color histogram)
