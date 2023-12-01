28b6306d3a15ca714f6cdd2a951944a1d40d1d31
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-01 10:17:09 +0200

    Update finaldefectapp_py.py

--- finaldefectapp_py.py
+++ finaldefectapp_py.py
@@ -1,76 +1,58 @@
-from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-import os
+import streamlit as st
+from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
-from keras.models import load_model
-
-app = Flask(__name__)
-
-# Set the upload folder and allowed extensions
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Load the metal classification model
-metal_model = load_model('classifyWaste.h5')
+metal_classification_model = load_model('/content/drive/My Drive/defect model/classifyWaste.h5')
 
-# Load the metal defect prediction model
-defect_model = load_model('mobilenet_model (1).h5')
+# Load the MobileNet defect prediction model
+defect_prediction_model = load_model('/content/drive/My Drive/defect model/mobilenet_model.h5')
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return redirect(request.url)
-    
-    file = request.files['file']
-    
-    if file.filename == '':
-        return redirect(request.url)
-    
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-
-        # Check if the uploaded image is metal or not
-        is_metal = predict_metal(file_path)
-
-        if is_metal:
-            # If it's metal, predict the defect
-            defect_prediction = predict_defect(file_path)
-            return render_template('result.html', result=f'This is a metal with defect: {defect_prediction}')
-        else:
-            return render_template('result.html', result='This is not a metal.')
-
-    return redirect(request.url)
-
-def predict_metal(image_path):
+# Function to predict metal and defect
+def predict_metal_and_defect(image_path):
+    # Load and preprocess the image for metal classification
     img = image.load_img(image_path, target_size=(150, 150))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    result = metal_model.predict(img_array)
-    return result[0][0] > 0.5  # Assuming a threshold of 0.5 for classification
+    img_array /= 255.0
 
-def predict_defect(image_path):
-    img = image.load_img(image_path, target_size=(150, 150))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    result = defect_model.predict(img_array)
-    class_index = np.argmax(result)
+    # Predict metal class
+    metal_prediction = metal_classification_model.predict(img_array)
+
+    # Check if it's a metal
+    is_metal = metal_prediction[0][0] > 0.5  # Adjust the threshold if needed
+
+    if is_metal:
+        # Load and preprocess the image for defect prediction
+        img = image.load_img(image_path, target_size=(150, 150))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.0
+
+        # Predict defect class
+        defect_prediction = defect_prediction_model.predict(img_array)
+        defect_class = np.argmax(defect_prediction)
+
+        return "Metal", defect_class
+    else:
+        return "Non-Metal", None
+
+# Streamlit app
+st.title("Metal and Defect Prediction App")
+
+# Upload image through Streamlit
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    # Display the uploaded image
+    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+
+    # Make predictions
+    metal_label, defect_label = predict_metal_and_defect(uploaded_file)
+
+    # Display predictions
+    st.write(f"Predicted Metal or Non-Metal: {metal_label}")
     
-    # Assuming your classes are in the following order
-    classes = ['Scratches', 'Patches', 'Rolled', 'Pitted', 'Inclusion', 'Crazing']
-    predicted_class = classes[class_index]
-
-    return predicted_class
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    if defect_label is not None:
+        st.write(f"Predicted Defect Class: {defect_label}")
